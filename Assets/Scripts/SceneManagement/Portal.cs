61d21fda883d2fd7068d26cefff36f5a18cc8739
dariusfuller <darius.fuller@lifeschools.org>
2021-02-05 10:56:27 -0600

    7.7 Cross scene reference
    Connected portals,made it to where i can leave a level then go back to a previous level if needed.

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        enum Destinationidentifier
+        {
+            A,B,C,D,E
+        }
+
+        [SerializeField] int sceneToLoad = 1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] Destinationidentifier destiniation;
+        private void OnTriggerEnter(Collider other)
+        {
+          if (other.tag == "Player" )
+            {
+                StartCoroutine(Transition());
+            }
+        }
+        private IEnumerator Transition()
+        {
+
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Sceneto load not set.");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destiniation != destiniation) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+    }
+}
