46cdf3775201ec86e28f8538a04e3b9065f50eaa
dariusfuller <darius.fuller@lifeschools.org>
2021-02-22 11:12:58 -0600

    7.10 Nested Couritnes For Fading
    Implemented fading.NOT Done.Start at 7:43

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOut(3f));
+        }
+        IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha != 1) 
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+
+}
