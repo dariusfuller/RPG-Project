0992794c28a803c4401d3dca5f2ccdce0312585b
dariusfuller <darius.fuller@lifeschools.org>
2021-03-01 11:13:00 -0600

    Savable Components and GameObjects
    Not finished resume at 7:33

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSavefile = "save";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Load(defaultSavefile);
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSavefile);
+        }
+    }
+}
