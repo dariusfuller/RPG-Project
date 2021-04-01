da7dcde107f0305184b9b53686ec0174eec7cd57
dariusfuller <darius.fuller@lifeschools.org>
2021-04-01 11:12:54 -0500

    10.9 Make a Weapon Projectile
    Move the arrow projectile (Start at 5:29)

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] Transform target = null;
+    [SerializeField] float speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+        transform.LookAt(target.position);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
