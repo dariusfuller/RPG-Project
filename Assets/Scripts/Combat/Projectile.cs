4a6e1d5b8151c3d95a0f3c2b3cba11070e2f9d9d
dariusfuller <darius.fuller@lifeschools.org>
2021-04-09 11:10:12 -0500

    10.10 Shoot Projectiles
     Setup our fighter class for our projectile
    Setup our weapon class to instantiate the projectile
    Setup our projectile class
    (unable to see arrow projectile)

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using RPG.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] Transform target = null;
+   
     [SerializeField] float speed = 1;
+    Health target = null;
 
     // Update is called once per frame
     void Update()
     {
         if (target == null) return;
 
-        transform.LookAt(target.position);
+        transform.LookAt(GetAimLocation());
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
+
+    public void SetTarget(Health target)
+    {
+        this.target = target;
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+        if (targetCapsule == null)
+        {
+            return target.transform.position;
+        }
+        return target.transform.position + Vector3.up * targetCapsule.height / 2;
+    }
+
 }
