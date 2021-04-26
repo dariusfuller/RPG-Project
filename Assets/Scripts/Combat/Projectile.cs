1789e0ff073ac48d331d9ba42ffb87759ce125cc
dariusfuller <darius.fuller@lifeschools.org>
2021-04-26 11:13:33 -0500

    10.15 Improve Projectile Behaviour
     Add heat seeking projectiles and fix issues with our projectile not following the behavior we want Create a homing option that can be checked
    Fix collision of our projectiles in our dead targets
    .

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -1,11 +1,10 @@
-﻿using RPG.Core;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using RPG.Core;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-   
     [SerializeField] float speed = 1;
     [SerializeField] bool isHoming = true;
     Health target = null;
@@ -16,13 +15,14 @@ public class Projectile : MonoBehaviour
         transform.LookAt(GetAimLocation());
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (target == null) return;
-        if (isHoming)
+        if (isHoming && !target.IsDead())
         {
-            //transform.LookAt(GetAimLocation());
+            {
+                transform.LookAt(GetAimLocation());
+            }
         }
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
@@ -46,6 +46,7 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Health>() != target) return;
+        if (target.IsDead()) return;
         target.TakeDamage(damage);
         Destroy(gameObject);
     }
